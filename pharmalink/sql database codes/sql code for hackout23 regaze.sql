cf44bc7b71816c22ffc95f85cf6bc9362b9b5982
demonwithinme <83051430+demonwithinme@users.noreply.github.com>
2023-08-27 09:13:47 +0530

    Delete pharmalink directory

--- pharmalink/sql database codes/sql code for hackout23 regaze.sql
+++
@@ -1,3 +0,0 @@
-create database if not exists hackout;
-use hackout;
-create table if not exists retail(hsn_code varchar(7), medicine_name varchar(40), quantity int, stock int, rate int, amount int, discount int, taxable_value int, cgst int, sgst int, igst int, cess int, total int, demand varchar(4));
