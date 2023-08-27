1bcaa74e56edf484d16bf8208cc51da274769438
demonwithinme <83051430+demonwithinme@users.noreply.github.com>
2023-08-27 09:07:58 +0530

    Update sql code for hackout23 regaze.sql

--- HackOut'23/sql database codes/sql code for hackout23 regaze.sql
+++ HackOut'23/sql database codes/sql code for hackout23 regaze.sql
@@ -1,3 +1,3 @@
 create database if not exists hackout;
 use hackout;
-create table if not exists retail(hsn_code varchar(7), medicine_name varchar(40), quantity int, stock int, rate int, amount int, discount int, taxable_value int, cgst int, sgst int, igst int, cess int, total int, demand varchar(4));
+create table if not exists retail(hsn_code varchar(7), medicine_name varchar(40), quantity int, stock int, rate int, amount int, discount int, taxable_value int, cgst int, sgst int, igst int, cess int, total int, demand int);
